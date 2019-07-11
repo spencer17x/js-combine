0c82ebd74ffd8d98fdd87b43a3a6a8ef233431fc
Code.X <1253478653@qq.com>
2019-07-11 14:07:15 +0800

    test: 测试包

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,2 @@
-declare const _default: "/**\n * url 参数拼接\n * @param object\n * @returns {string}\n */\nexport const combineUrlParams: (object: object) => string = (object: object = {}) => {\n  return Object.entries(object) // 生成二维数组\n    .filter((arr: any[]) => arr[1]) // 过滤假值\n    .map((arr: any[]) => arr.join('=')) // 拼接参数，转为一维数组\n    .join('&') // 转为字符串\n}\n";
+declare const _default: "/**\n * url 参数拼接\n * @param object\n * @returns {string}\n */\nexport function combineUrlParams(object: object) {\n  return Object.entries(object) // 生成二维数组\n    .filter((arr: any[]) => arr[1]) // 过滤假值\n    .map((arr: any[]) => arr.join('=')) // 拼接参数，转为一维数组\n    .join('&') // 转为字符串\n}\n";
 export default _default;
