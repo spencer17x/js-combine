b2668d113a18231cbe308b3759b72cd90d641766
Code.X <1253478653@qq.com>
2019-07-11 11:21:17 +0800

    refactor: 使用webpack构建

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * url 参数拼接
- * @param object
- * @returns {string}
- */
-export declare const combineUrlParams: (object: object) => string;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const modName = 'js-combine';
+
+module.exports = {
+  mode: "production",
+  entry: './src/index.ts',
+  output: {
+    filename: `${modName}.js`,
+    library: modName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      { test: /\.ts?$/, use: "awesome-typescript-loader" },
+      { test: /\.ts?$/, use: "raw-loader" }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      dry: true,
+      verbose: true
+    })
+  ]
+}
