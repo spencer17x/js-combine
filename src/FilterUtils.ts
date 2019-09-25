0b84c25d3da1f5973a380b367363641f474299a8
Code.X <1253478653@qq.com>
2019-09-25 20:15:01 +0800

    feat: json.parse过滤指定值

--- src/FilterUtils.ts
+++ src/FilterUtils.ts
@@ -10,11 +10,16 @@ export default {
    * @param array - 过滤指定的值
    */
   filterSpecifiedValue(object: IObject, array: any[]): object {
-    Object.keys(object).forEach((key: string) => {
-      if (array.includes(object[key])) {
-        delete object[key]
+    // Object.keys(object).forEach((key: string) => {
+    //   if (array.includes(object[key])) {
+    //     Reflect.deleteProperty(object, key)
+    //   }
+    // })
+    return JSON.parse(JSON.stringify(object, (k: string, v: any) => {
+      if (array.includes(v)) {
+        return undefined;
       }
-    })
-    return object
+      return v;
+    }));
   },
 }
