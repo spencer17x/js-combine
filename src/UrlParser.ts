8312cdf4f35475a7d7abe4c024a56696f1064650
Code.X <1253478653@qq.com>
2020-03-26 23:10:12 +0800

    refactor(删除不要的方法):

--- src/UrlParser.ts
+++ src/UrlParser.ts
@@ -1,16 +1,16 @@
 export default {
-  /**
-   * 主要为了实现浏览器中获取参数
-   * 获取url中的参数
-   * @param name
-   * @param 浏览器环境下urlSearch 应设置为 location.search
-   */
-  getQueryString: (name: string = '', urlSearch: string = ''): string | null => {
-    const reg: string = '(^|&)' + name + '=([^&]*)(&|$)'
-    const r: RegExpMatchArray | null = urlSearch.substr(1).match(reg)
-    if (r !== null) {
-      return unescape(r[2])
-    }
-    return null
-  },
-}
+	/**
+	 * 主要为了实现浏览器中获取参数
+	 * 获取url中的参数
+	 * @param name
+	 * @param 浏览器环境下urlSearch 应设置为 location.search
+	 */
+	getQueryString: (name: string = '', urlSearch: string = ''): string | null => {
+		const reg: string = '(^|&)' + name + '=([^&]*)(&|$)';
+		const r: RegExpMatchArray | null = urlSearch.substr(1).match(reg);
+		if (r !== null) {
+			return unescape(r[2]);
+		}
+		return null;
+	},
+};
