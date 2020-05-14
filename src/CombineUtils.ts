4111be15092250d53569e0002295889155a04c7a
Spencer17x <1253478653@qq.com>
2020-05-14 22:22:40 +0800

    fix(修改lock文件):

--- src/CombineUtils.ts
+++ src/CombineUtils.ts
@@ -1,20 +1,20 @@
 export default {
-	/**
-	 * url 参数拼接
-	 * @param object
-	 * @returns {string}
-	 */
-	combineUrlParams: (object: object = {}): string => {
-		return Object.entries(object) // 生成二维数组
-		.filter((arr: any[]) => ![undefined, null, NaN].includes(arr[1])) // 过滤假值
-		.map((arr: any[]) => arr.join('=')) // 拼接参数，转为一维数组
-		.join('&'); // 转为字符串
-	},
-	/**
-	 * 合并className函数
-	 * @param args
-	 */
-	combineClass: (...args: Array<string | undefined>): string => {
-		return args.filter(Boolean).join(' ');
-	},
-};
+  /**
+   * url 参数拼接
+   * @param object
+   * @returns {string}
+   */
+  combineUrlParams: (object: object = {}): string => {
+    return Object.entries(object) // 生成二维数组
+      .filter((arr: any[]) => ![undefined, null, NaN].includes(arr[1])) // 过滤假值
+      .map((arr: any[]) => arr.join('=')) // 拼接参数，转为一维数组
+      .join('&') // 转为字符串
+  },
+  /**
+   * 合并className函数
+   * @param args
+   */
+  combineClass: (...args: Array<string | undefined>): string => {
+    return args.filter(Boolean).join(' ')
+  },
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export { default as ArrayUtils } from './ArrayUtils'
 export { default as CombineUtils } from './CombineUtils'
 export { default as UrlParser } from './UrlParser'
 export { default as FilterUtils } from './FilterUtils'
